bfc0d0e06ff6baaf27751e84140bfa9b48f099b4
xupeng-faker <1372702517@qq.com>
2026-03-04 19:01:54 +0800

    代码提交

--- agent_v2/config.py
+++ agent_v2/config.py
@@ -1,5 +1,5 @@
 import os
 
-FAKE_APP_BASE_URL = os.getenv("FAKE_APP_BASE_URL", "http://localhost:8080")
+FAKE_APP_BASE_URL = os.getenv("FAKE_APP_BASE_URL", "http://7.225.29.223:8080")
 MODEL_PORT = 8888
 USER_ID = os.getenv("USER_ID", "w30020094")
